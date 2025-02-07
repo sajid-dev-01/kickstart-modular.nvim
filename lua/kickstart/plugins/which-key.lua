dedf604ad8083a8d1029592c4795f6fe1b594a72
sajid-dev-01 <sajid.dev.01@gmail.com>
2025-02-07 10:02:13 +0600

    feat(which-key): update which key preset

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -17,6 +17,7 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+      preset = 'helix',
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
