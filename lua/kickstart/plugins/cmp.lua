fe0d94cbf17d3230c4744e43da6e77ab8b0fa2bb
sajid-dev-01 <sajid.dev.01@gmail.com>
2025-04-05 22:00:15 +0600

    fix(cmp.lua): fix cmp.lua

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -164,10 +164,10 @@ return {
       })
       cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
+        sources = cmp.config.sources {
           { name = 'path' },
         },
-      }
+      })
     end,
   },
 }
