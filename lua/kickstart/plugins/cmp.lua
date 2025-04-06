ccaf93cb29e8cfcf35c113ffc42da172a2811669
sajid-dev-01 <sajid.dev.01@gmail.com>
2025-04-06 09:15:17 +0600

    fix(cmp.lua): fix cmdline completion

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -160,13 +160,16 @@ return {
 
       cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = { { name = 'buffer' } },
+        sources = cmp.config.sources { { name = 'buffer' } },
       })
       cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources {
+        sources = cmp.config.sources({
           { name = 'path' },
-        },
+        }, {
+          { name = 'cmdline' },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
       })
     end,
   },
